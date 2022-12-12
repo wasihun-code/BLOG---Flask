37301cca1148c97bc9173c6d043235f2764d2ef2
wasihun-code <wasihunageru7947@gmail.com>
2022-12-13 00:25:30 +0530

    Added secret key for csrf

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, url_for
 
-
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'e2f679bf0980230e224a745c'
+
 posts = [
     {
         'author': 'Corey Schafer',
