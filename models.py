0c51bb6834994627151abd4cc8ccae6b6d63984a
wasihun-code <wasihunageru7947@gmail.com>
2022-12-13 11:20:42 +0530

    ADD :models        :User -> represents a user in the website        :Post -> represents a post in the website

---
+++ models.py
@@ -0,0 +1,32 @@
+from app import app
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy(app)
+app.app_context().push()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    image = db.Column(
+        db.String(20),
+        nullable=False,
+        default="default.jpg",
+    )
+    posts = db.relationship("Post", backref="author", lazy=True)
+
+    def __repr__(self) -> str:
+        return f" User('{self.username}', '{self.email}', '{self.image}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False, unique=True)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self) -> str:
+        return f" Post('{self.title}', '{self.date_posted}')"
