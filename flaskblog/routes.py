1b02588475f69dc922deae84b1a2e183c87286c6
wasihun-code <wasihunageru7947@gmail.com>
2022-12-14 10:41:02 +0530

    MODIFY :register route            :change password from plain text to HASHES            :add    flash success message on successful regestration            :redirect user to login page if successful registratoin            :render register.html on get request of registration failure MODIFY :login route           :change password from plain to HASHED           :add    login manager for the website                   flash messages on error and success           :recirect user to home if authenticated                          to account if authenticated and wanted to go to accounts page first ADD :logout route        :redirects user to login page     :account route        :template only        :later users will be able to edit, view and manage their account

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,8 @@
-from flaskblog import app
-from flask import render_template, url_for, flash, redirect
+from flaskblog import app, db, bcrypt
+from flask import render_template, url_for, flash, redirect, request
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
+from flask_login import login_user, logout_user, login_required
 
 posts = [
     {
@@ -20,7 +21,6 @@ posts = [
 
 
 @app.route("/")
-@app.route("/home")
 @app.route("/index")
 def index() -> str:
     return render_template("index.html", posts=posts)
@@ -34,13 +34,76 @@ def about() -> str:
 @app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
-    if form.validate_on_submit():
-        flash("Account created successfully for {form.username}!", category="success")
-        return redirect(url_for("home"))
+    
+    if request.method=="POST" and form.validate_on_submit():
+        # Debugging purpose
+        print("Form Validated")
+        
+        # Creating hash for password: security reasons
+        hashed_pw = bcrypt.generate_password_hash(form.password.data)
+        
+        # Instantiating a user object to save to databse
+        user = User(username=form.username.data, email=form.email.data, password=hashed_pw)
+        
+        # Adding user to database
+        db.session.add(user)
+        db.session.commit()
+        
+        # Display success message
+        flash("Account created successfully! You can now Login", category="success")
+        
+        # Redirect them to login page
+        return redirect(url_for("login"))
+    
+    # On get request, create a form object and render the template for users to register
     return render_template("register.html", title="Register to get started", form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login() -> str:
+    
+    # Create a login form instance
     form = LoginForm()
-    return render_template("login.html", title="Login to BLOG", form=form)
+    
+    # if all fields of the form are correct, then it is validated
+    if form.validate_on_submit():
+        
+        # Query the user by the entered username
+        user = User.query.filter_by(username=form.username.data).first()
+        
+        # if the query returned a result
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            
+            # then login to the website and display message
+            login_user(user=user, remember=form.remember.data)
+            flash("You've Logged in successfully", 'success')
+            
+            # If user wanted to login to account directly
+            if request.args.get('next') == '/account':
+                
+                # Debugging purpose
+                print(request.args.get('next'))
+                
+                # redirect them to the account route
+                return redirect(url_for("account"))
+
+            # redirect them to home page
+            return redirect(url_for("index"))
+        
+        # if no user exists with the entered credentials
+        flash("Login Unsuccessful. Please check username and password and try again.", "danger")
+        
+    # either in get request of failure to provide correct credential serve the template
+    return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title="Account")
