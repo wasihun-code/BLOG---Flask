1e516a6b05f1b0d312163124dd70f2fd77cf3ac2
wasihun-code <wasihunageru7947@gmail.com>
2022-12-15 11:45:33 +0530

    routes.py   Modify route index    : Posts are ordered in such a way that latest come first   Create route user_posts  : Filters post by user  : Posts are ordered  : Pagination included index.html   Change username links     : now username links redirect to users posts post.html   Change username links     : now username links redirect to users posts user_post.html   Create template     : displays individual user posts     : with the number of posts they have and their username as heading     : display pagination

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -26,7 +26,7 @@ posts = [
 @app.route("/index")
 def index() -> str:
     page = request.args.get("page", 1, type=int)
-    posts = Post.query.paginate(per_page=5, page=page)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=5, page=page)
     return render_template("index.html", posts=posts)
 
 
@@ -243,3 +243,15 @@ def delete_post(post_id):
     db.session.commit()
     flash("Your post has been deleted", category="success")
     return redirect(url_for("index"))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get("page", 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = (
+        Post.query.filter_by(author=user)
+        .order_by(Post.date_posted.desc())
+        .paginate(page=page, per_page=2)
+    )
+    return render_template("user_post.html", posts=posts, user=user)
