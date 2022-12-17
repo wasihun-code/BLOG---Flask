6e0377b543ca3b6fd2b451fe3c5d4b7f09f26307
wasihun-code <wasihunageru7947@gmail.com>
2022-12-17 21:50:33 +0530

    errors_403.html -> NEW errors_404.html -> NEW errors_500.html -> NEW       - Templates for custom error pages __init__.py       Register errors blueprint request_password.html     Fix main-content section mixing with sidebar

---
+++ flaskblog/errors/handlers.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+
+errors = Blueprint('errors', __name__)
+
+@errors.app_errorhandler(404)
+def error_404(error):
+    return render_template('errors/error_404.html'), 404
+
+@errors.app_errorhandler(403)
+def error_404(error):
+    return render_template('errors/error_403.html'), 403
+
+@errors.app_errorhandler(500)
+def error_404(error):
+    return render_template('errors/error_500.html'), 500
